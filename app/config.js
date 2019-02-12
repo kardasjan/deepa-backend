c2868977c9dc07da365f9b9579cf7f9484495325
Jan Kardaš <jan.kardas@seznam.cz>
2019-02-12 13:05:18 +0100

    Change default DB

--- app/config.js
+++ app/config.js
@@ -1,7 +1,7 @@
 export default {
   PROJECT_PORT: '3015',
   PROJECT_URL: 'http://localhost:3015',
-  MONGO_URL: 'mongodb://localhost:27017/deepa',
+  MONGO_URL: 'mongodb://localhost:27017/watch',
   MONGO_TEST_URL: 'mongodb://localhost:27017/deepa_test',
   WEB_APIKEY: '03d9ad2076ebfa3edb9fec0a4cd6c843',
   JWT_SECRET: 'awesomesecretcodewithlove',
